58676a77d11c5cf81288615399c4b321082467b5
Jason Kwon <jinsan.kwon@gmail.com>
2020-09-23 15:42:30 +0900

    Switching logics (in prgs.)

--- attiny1614_accsupervisor/main.c
+++ attiny1614_accsupervisor/main.c
@@ -32,7 +32,13 @@ static uint8_t _gpio_status = 0;
 // ...we have to send the raw values and let the counterpart(TX2) do the math.
 uint16_t vbat_volt = 0;
 uint16_t vacc_volt = 0;
-uint8_t adc_state = 0;
+//uint8_t adc_state = 0;
+// Low battery cut-off threshold, default: 10.5v (588)
+uint16_t vbat_threshold = 588;
+// Accessory power detection threshold, default: 10.5v (588)
+uint16_t vacc_threshold = 588;
+// Accessory power cutoff holding time, default: 3 ticks
+uint8_t vacc_cutoff_hold = 3;
 
 volatile uint8_t sleep_requested = 1;
 
@@ -234,13 +240,13 @@ void doAdcThings()
 			{
 				// VBAT
 				vbat_volt = ADC0.RES;
-				adc_state |= 0x1;
+				//adc_state |= 0x1;
 			}
 			if((ADC0.MUXPOS & ADC_MUXPOS_gm) == ADC_MUXPOS_AIN2_gc)
 			{
 				// VACC
 				vacc_volt = ADC0.RES;
-				adc_state |= 0x2;
+				//adc_state |= 0x2;
 			}
 			ADC0.INTFLAGS = (1 & ADC_RESRDY_bm);
 		}
@@ -255,6 +261,28 @@ void doAdcThings()
 		}
 		ADC0.COMMAND = (1 & ADC_STCONV_bm);
 	}
+}
+
+void doSwitchingThings(void)
+{
+	if(isRelayOn())
+	{
+		// Off logics
+		;
+	}
+	else
+	{
+		// On logics
+		
+		// Relay is always on if the accessory power presents
+		if(vacc_volt >= vacc_threshold) relayOn();
+	}
+}
+
+void doTx2CmdThings(void)
+{
+	// a) BAT is okay & accessory is off -> Send 'Backup' command
+	// b) BAT is nokay & accessory is off -> Send 'Halt' command
 }
 
 int main(void)
@@ -278,16 +306,11 @@ int main(void)
 	adcOn();
 	
 	// Static scheduling loop
+	// TODO: Scheduling. Consider UART parser
     while (1)
     {
 		doAdcThings();
-		if((adc_state & 0x3) == 0x3)
-		{
-			// TODO: Scheduling. Consider UART parser
-			printf("VBAT: %d, VACC: %d\r\n", vbat_volt, vacc_volt);
-			adc_state = 0;
-			sleep_requested = 1;
-		}
+		doTx2CmdThings();
 
 		if(uart0_rbuf_rpnt != uart0_rbuf_wpnt)
 		{
